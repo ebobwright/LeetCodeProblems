c6448863a82810dd9d578bc4af00e9b1d94d411f
Bob Wright <ebobwright@gmail.com>
2024-01-04 14:45:25 -0800

    Add solution for RotateArray

---
+++ LeetCodeProblems/RotateArraySolution.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.com/problems/rotate-array
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class RotateArraySolution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            if(k > nums.Length)
+            {
+                k = k % nums.Length;
+            }
+
+            var pivotPoint = nums.Length - k;
+            var secondHalf = nums.Skip(pivotPoint).Take(nums.Length - pivotPoint).ToArray();
+            var firstHalf = nums.Skip(0).Take(pivotPoint).ToArray();
+            
+            secondHalf.CopyTo(nums, 0);
+            firstHalf.CopyTo(nums, secondHalf.Length);
+        }
+    }
+}
---
+++ LeetCodeTests/RotateArrayTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeProblems;
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public class RotateArrayTest
+    {
+        [TestMethod()]
+        public void LeetCode_RotateArray_Test1()
+        {
+            //Arrange
+            var target = new RotateArraySolution();
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            int k = 3;
+
+            //Act
+            target.Rotate(nums, k);
+
+            //Assert                        
+            CollectionAssert.AreEqual(new int[] { 5, 6, 7, 1, 2, 3, 4 }, nums);
+        }
+
+        [TestMethod()]
+        public void LeetCode_RotateArray_Test2()
+        {
+            //Arrange
+            var target = new RotateArraySolution();
+            int[] nums = { -1, -100, 3, 99 };
+            int k = 2;
+
+            //Act
+            target.Rotate(nums, k);
+
+            //Assert                        
+            CollectionAssert.AreEqual(new int[] { 3, 99, -1, -100 }, nums);
+        }
+
+        [TestMethod()]
+        public void LeetCode_RotateArray_Test3()
+        {
+            //Arrange
+            var target = new RotateArraySolution();
+            int[] nums = { 1, 2 };
+            int k = 3;
+
+            //Act
+            target.Rotate(nums, k);
+
+            //Assert                        
+            CollectionAssert.AreEqual(new int[] { 2,1 }, nums);
+        }
+    }
+}
