c53688f568aa741a1492779e86d34c90711e035b
Bob Wright <ebobwright@gmail.com>
2024-01-05 15:32:20 -0800

    playing with data driven testing!

--- LeetCodeTests/RotateArrayTest.cs
+++ LeetCodeTests/RotateArrayTest.cs
@@ -6,49 +6,20 @@ namespace LeetCodeTests
     [TestClass]
     public class RotateArrayTest
     {
-        [TestMethod()]
-        public void LeetCode_RotateArray_Test1()
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3, new int[] { 5, 6, 7, 1, 2, 3, 4 })]
+        [DataRow(new int[] { -1, -100, 3, 99 }, 2, new int[] { 3, 99, -1, -100 })]
+        [DataRow(new int[] { 1, 2 }, 3, new int[] { 2, 1 })]
+        public void LeetCode_RotateArray_DDT(int[] nums, int k, int[] exp)
         {
             //Arrange
             var target = new RotateArraySolution();
-            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
-            int k = 3;
-
+          
             //Act
             target.Rotate(nums, k);
 
             //Assert                        
-            CollectionAssert.AreEqual(new int[] { 5, 6, 7, 1, 2, 3, 4 }, nums);
-        }
-
-        [TestMethod()]
-        public void LeetCode_RotateArray_Test2()
-        {
-            //Arrange
-            var target = new RotateArraySolution();
-            int[] nums = { -1, -100, 3, 99 };
-            int k = 2;
-
-            //Act
-            target.Rotate(nums, k);
-
-            //Assert                        
-            CollectionAssert.AreEqual(new int[] { 3, 99, -1, -100 }, nums);
-        }
-
-        [TestMethod()]
-        public void LeetCode_RotateArray_Test3()
-        {
-            //Arrange
-            var target = new RotateArraySolution();
-            int[] nums = { 1, 2 };
-            int k = 3;
-
-            //Act
-            target.Rotate(nums, k);
-
-            //Assert                        
-            CollectionAssert.AreEqual(new int[] { 2,1 }, nums);
+            CollectionAssert.AreEqual(exp, nums);
         }
     }
 }
