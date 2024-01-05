47f82919d5cfdac0e6c077d1d758e5002543644d
Bob Wright <ebobwright@gmail.com>
2024-01-05 15:15:43 -0800

    Did "hard" problem, Trapping Rain Water

---
+++ LeetCodeProblems/TrappingRainWaterSolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCodeProblems
+{
+    public class TrappingRainWaterSolution
+    {
+        public int Trap(int[] height)
+        {
+            // pre-record highest values to the left
+            int[] hvls = new int[height.Length];
+            hvls[0] = 0;
+            for(int i = 1; i < height.Length; i++)
+            {
+                hvls[i] = Math.Max(height[i - 1], hvls[i - 1]);     
+            }
+
+            // pre-record highest values to the right
+            int[] hvrs = new int[height.Length];
+            hvrs[height.Length - 1] = 0;
+            for (int i = height.Length - 2; i >= 0; i--)
+            {
+                hvrs[i] = Math.Max(height[i + 1], hvrs[i + 1]);
+            }
+
+            var totalWater = 0;
+            var currentMaxHeight = 0;
+            //check every vertical stack of "water" aside from first and last index becasue no water can pool there
+            for(int i = 1; i < height.Length - 1; i++)
+            {
+                if(hvls[i] > height[i] && hvrs[i] > height[i])
+                {
+                    totalWater += Math.Min(hvls[i], hvrs[i]) - height[i];
+                }                
+            }
+            return totalWater;
+        }                
+    }
+}
---
+++ LeetCodeTests/TrappingRainWaterTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeProblems;
+
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public class TrappingRainWaterTest
+    {
+        [TestMethod()]
+        public void LeetCode_TrappingRainWater_Test1()
+        {
+            //Arrange
+            var target = new TrappingRainWaterSolution();
+            int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            
+
+            //Act
+            var ret = target.Trap(height);
+
+            //Assert
+            Assert.AreEqual(6, ret);
+        }
+
+        [TestMethod()]
+        public void LeetCode_TrappingRainWater_Test2()
+        {
+            //Arrange
+            var target = new TrappingRainWaterSolution();
+            int[] height = { 4, 2, 0, 3, 2, 5 };
+
+
+            //Act
+            var ret = target.Trap(height);
+
+            //Assert
+            Assert.AreEqual(9, ret);
+        }
+    }
+}
